d9510c714488f3a4234d0b8464ca636ec4b397f0
Gvensy322 <itmm103gorvengarciasalaveria@gmail.com>
2018-07-11 11:28:07 +0800

    Contents of Section G

---
+++ SectionG.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>PHP 5 Data Types</h1>
+
+<h2>PHP Data Types</h2>
+<p>Variables can store data of different types, and different data types can do different things.</p>
+<p>PHP supports the following data types:</p>
+<p>String</p>
+<p>Integer</p>
+<p>Float (floating point numbers - also called double)</p>
+<p>Boolean</p>
+<p>Array</p>
+<p>Object</p>
+<p>NULL</p>
+<p>Resource</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+
+<a href=https://www.w3schools.com/pHP/php_datatypes.asp>More about Data Types</a>
+
+
+
+
+</body>
+</html>
