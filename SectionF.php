f2ef7e66dbd94b979a124a33a3da2d6c96688141
Gvensy322 <itmm103gorvengarciasalaveria@gmail.com>
2018-07-11 11:26:16 +0800

    Contents of Section F

---
+++ SectionF.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>PHP 5 echo and print Statements</h1>
+
+<h>In PHP there are two basic ways to get output: echo and print.</p>
+<p>In this tutorial we use echo (and print) in almost every example. So, this chapter contains a little more info about those two output statements.</p>
+
+<h2>PHP echo and print Statements</h2>
+
+<p>echo and print are more or less the same. They are both used to output data to the screen.</p>
+
+<p>The differences are small: echo has no return value while print has a return value of 1 so it can be used in expressions
+<p>Echo can take multiple parameters (although such usage is rare) while print can take one argument.</p>
+<p>Echo is marginally faster than print.</p>
+
+
+
+<a href=https://www.w3schools.com/pHP/php_echo_print.asp>See More Sample of Echo and Print Statement of PHP</a>
+
+
+
+
+</body>
+</html>
