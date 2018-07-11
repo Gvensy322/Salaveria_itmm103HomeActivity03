0bd9675b34d164e985d9e1327824bb8666cfbaaa
Gvensy322 <itmm103gorvengarciasalaveria@gmail.com>
2018-07-11 11:32:48 +0800

    Contents of Section I

---
+++ SectionI.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>PHP 5 Constants</h1>
+
+<p>Constants are like variables except that once they are defined they cannot be changed or undefined.</p>
+
+<h2>PHP Constants</h2>
+
+
+<p>A constant is an identifier (name) for a simple value. The value cannot be changed during the script.</p>
+<p>A valid constant name starts with a letter or underscore (no $ sign before the constant name).</p>
+<p>Note: Unlike variables, constants are automatically global across the entire script.
+</p>
+
+
+<a href=https://www.w3schools.com/pHP/php_constants.asp>More about Constants and Learn More about PHP</a>
+
+
+
+
+</body>
+</html>
