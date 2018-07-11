f2dd1d3827bcf8cad61088eeb809ca750318a285
Gvensy322 <itmm103gorvengarciasalaveria@gmail.com>
2018-07-11 11:05:56 +0800

    Contents of Section B

---
+++ SectionB.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+
+<h1>PHP 5 Introduction</h1>
+<h>PHP scripts are executed on the server.</h>
+<p>What You Should Already Know</p>
+
+<h>Before you continue you should have a basic understanding of the following:HTML
+-CSS
+-JavaScript</h>
+
+
+<p>If you want to study these subjects first, find the tutorials on our</p><a href=https://www.w3schools.com/pHP/default.asp>Home Page</a>
+
+<h2>What is PHP?</h2>
+<p>PHP is an acronym for "PHP: Hypertext Preprocessor"</p>
+<p>PHP is a widely-used, open source scripting language</p>
+<p>PHP scripts are executed on the server</p>
+<p>PHP is free to download and use</p>
+
+<h>PHP is an amazing and popular language!</h>
+<p>It is powerful enough to be at the core of the biggest blogging system on the web (WordPress)!</p>
+<p>It is deep enough to run the largest social network (Facebook)!</p>
+<p>It is also easy enough to be a beginner's first server side lan
