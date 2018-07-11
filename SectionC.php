90f475c82f270fc72c1183e18c249ac5a50e62ea
Gvensy322 <itmm103gorvengarciasalaveria@gmail.com>
2018-07-11 11:12:07 +0800

    Contents of Section C

---
+++ SectionC.php
@@ -0,0 +1,35 @@
+!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+<h1>PHP 5 Installation</h1>
+<h>What Do I Need?</h>
+<p>To start using PHP, you can:
+
+Find a web host with PHP and MySQL support
+Install a web server on your own PC, and then install PHP and MySQL</p>
+
+<h>Use a Web Host With PHP Support</h>
+<p>If your server has activated support for PHP you do not need to do anything.
+
+Just create some .php files, place them in your web directory, and the server will automatically parse them for you.
+
+You do not need to compile anything or install any extra tools.
+
+Because PHP is free, most web hosts offer PHP support.</p>
+
+<h>Set Up PHP on Your Own PC</h>
+<p>However, if your server does not support PHP, you must:
+-install a web server
+-install PHP
+-install a database, such as MySQL</p>
+
+<a href=http://php.net/manual/en/install.php>The official PHP website (PHP.net) has installation instructions for PHP:</a>
+
+
+
+
+</body>
+</html>
