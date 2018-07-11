aa90229229431e03ae253c3202a132db0625698e
Gvensy322 <itmm103gorvengarciasalaveria@gmail.com>
2018-07-11 11:30:44 +0800

    Contents of Section H

---
+++ SectionH.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>PHP 5 Strings</h1>
+
+<p>A string is a sequence of characters, like "Hello world!".</p>
+
+<h2>PHP String Functions</h2>
+
+
+<p>In this chapter we will look at some commonly used functions to manipulate strings.</p>
+
+
+
+<a href=https://www.w3schools.com/pHP/php_string.asp>More about String</a>
+
+
+
+
+</body>
+</html>
