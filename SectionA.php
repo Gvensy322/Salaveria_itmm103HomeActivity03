7100c2860bce5ee867b003724f79ee5986851ea8
Gvensy322 <itmm103gorvengarciasalaveria@gmail.com>
2018-07-11 11:02:57 +0800

    Contents of Section A

---
+++ SectionA.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>PHP 5 Tutorial</h1>
+
+<h>PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.</h>
+<p>PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.</p>
+
+<h>Easy Learning with "Show PHP"</h>
+<p>Our "Show PHP" tool makes it easy to learn PHP, it shows both the PHP source code and the HTML output of the code</p>
+<a href=https://www.w3schools.com/pHP/default.asp>This is the PHP Tutorial</a>
+
+
+
+
+</body>
+</html>
