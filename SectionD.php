b27f538158fc377e62fac5a210a4ac0721206ac1
Gvensy322 <itmm103gorvengarciasalaveria@gmail.com>
2018-07-11 11:20:47 +0800

    Contents of Section D

---
+++ SectionD.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>PHP 5 Syntax</h1>
+
+<h>A PHP script is executed on the server, and the plain HTML result is sent back to the browser.</p>
+
+<h2>Basic PHP Syntax</h2>
+
+<p>A PHP script can be placed anywhere in the document.</p>
+
+<p>The default file extension for PHP files is ".php".</p>
+
+<p>A PHP file normally contains HTML tags, and some PHP scripting code.</p>
+<p>Below, we have an example of a simple PHP file, with a PHP script that uses a built-in PHP function "echo" to output the text "Hello World!" on a web page:</p>
+
+
+<a href=https://www.w3schools.com/pHP/php_syntax.asp>See Sample Syntax of PHP </a>
+
+
+
+
+</body>
+</html>
