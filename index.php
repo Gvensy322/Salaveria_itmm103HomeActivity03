b23699a41f6dd1e4fdf8bd7e827fbc6ee370ccef
Gvensy322 <itmm103gorvengarciasalaveria@gmail.com>
2018-07-11 10:56:21 +0800

    Contents with Links

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h>Activity Name:More on PHP Basics</h>
+<p>Name:Gorven G. Salaveria</p>
+<p>Course Title: Advance Web Develop,Third Year</p>
+<p>Activity Name:More on PHP Basics</p>
+
+<p>
+<a href=SectionA.php>Link to Section A</a>
+</p>
+
+<p>
+<a href=SectionB.php>Link to Section B</a>
+</p>
+
+<p>
+<a href=SectionC.php>Link to Section C</a>
+</p>
+
+<p>
+<a href=SectionD.php>Link to Section D</a>
+</p>
+
+<p>
+<a href=SectionE.php>Link to Section E</a>
+</p>
+
+
+<p>
+<a href=SectionF.php>Link to Section F</a>
+</p>
+
+
+<p>
+<a href=SectionG.php>Link to Section G</a>
+</p>
+
+
+<p>
+<a href=SectionH.php>Link to Section H</a>
+</p>
+
+<p>
+<a href=SectionI.php>Link to Section I</a>
+</p>
+
+
+</body>
+</html>
