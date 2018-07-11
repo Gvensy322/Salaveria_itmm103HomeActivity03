537dc26841c935fe1971d5b4c06f748502327bf8
Gvensy322 <itmm103gorvengarciasalaveria@gmail.com>
2018-07-11 11:23:35 +0800

    Contents of Section E

---
+++ SectionE.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>PHP 5 Variables</h1>
+
+<h>Variables are "containers" for storing information.</p>
+
+<h2>PHP Variables</h2>
+
+<p>A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).</p>
+
+<p>Rules for PHP variables:</p>
+<p>A variable starts with the $ sign, followed by the name of the.</p>
+<p>A variable name must start with a letter or the underscore character.</p>
+<p>A variable name cannot start with a number.</p>
+<p>A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).</p>
+<p>Variable names are case-sensitive ($age and $AGE are two different variables).</p>
+<p>Remember that PHP variable names are case-sensitive!</p>
+
+
+<a href=https://www.w3schools.com/pHP/php_variables.asp>See More Sample How to Declare Variable of PHP </a>
+
+
+
+
+</body>
+</html>
